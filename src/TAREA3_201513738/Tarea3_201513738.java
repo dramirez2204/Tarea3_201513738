65b813c94843bf0d937f1a3f90ad919e4577232e
HP CORE I5 <HP CORE I5@192.168.1.14>
2018-08-19 14:30:50 -0600

    Estoy Trabajando en mi Tarea 3

---
+++ src/TAREA3_201513738/Tarea3_201513738.java
@@ -0,0 +1,23 @@
+package TAREA3_201513738;
+import java.util.Scanner;
+public class Tarea3_201513738 {
+
+    public static void main(String[] args) { //Codigo para salir del sistema
+        Scanner entrada = new Scanner(System.in);
+        Menu objMenu=new Menu();
+        Funcion objFuncion=new Funcion();
+        int intFuncion=-1;
+        Boolean Salir=false;
+        while(Salir==false){
+            intFuncion=objMenu.ImprimeMenu(entrada);
+            if(intFuncion==1){
+                System.out.println("Usted ha salido del programa, Esperamos que regrese!."); //Mensaje Final
+                break;
+            }
+            objFuncion.ProcesarFuncion(intFuncion, entrada);
+        }
+        
+    }
+    
+}
+
